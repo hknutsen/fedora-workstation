b1bec815121ced6965a83809abd20970f62443e4
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2025-02-03 06:53:07 +0100

    refactor(audio): rip CD to static directory

--- audio/rip_cd.sh
+++ audio/rip_cd.sh
@@ -21,15 +21,17 @@ if [[ ! -d "$FLAC_DIR" ]]; then
 fi
 cd "$FLAC_DIR"
 
-date=$(date +"%Y%m%d%H%M%S")
-rip_dir="rip_$date"
-if [[ -d "$rip_dir" ]]; then
-  echo "Rip directory '$rip_dir' already exists! Should rip to empty directory to avoid overriding existing rip. Aborting..."
+rip_dir="cdparanoia"
+if [[ ! -d "$rip_dir" ]]; then
+  mkdir "$rip_dir"
+fi
+cd "$rip_dir"
+
+if [[ -n $(ls -A) ]]; then
+  echo "Directory '$rip_dir' is not empty! Aborting..."
   exit 1
 fi
-mkdir "$rip_dir"
-cd "$rip_dir"
 
 cdparanoia --batch --output-wav --abort-on-skip
 
-find . -name '*.wav' -type f | parallel --progress 'flac --best --silent --delete-input-file {}'
+find . -name '*.wav' -type f | parallel --progress 'flac --best --delete-input-file {}'
